7fa97d2378c3c3c09678cf9ed3a29e35f0d90cfe
PVD-Teacher <127093027+pvd-teacher@users.noreply.github.com>
2023-03-21 17:49:53 +0530

    Update KiparoCom.kt

--- 01 first test task/app/src/main/java/com/yakovsn/homework/KiparoCom.kt
+++ 01 first test task/app/src/main/java/com/yakovsn/homework/KiparoCom.kt
@@ -6,22 +6,26 @@ import java.net.URL
 class KiparoCom : GetFromNetwork {
 
     override fun getJson(): String? {
+        //private val JSON_URL
         val url = URL("https://api2.kiparo.com/static/it_news.json")
         val urlConnection = url.openConnection() as HttpURLConnection
         return try {
             urlConnection.inputStream.bufferedReader().readText()
         } catch (t: Throwable) {
+//          No UI interactions here, simply throw
             println("соединение отстутсвует")
             null
         }
     }
 
     override fun getXML(): String? {
+         //private val XML_URL
         val url2 = URL("https://api2.kiparo.com/static/it_news.xml")
         val urlConnection2 = url2.openConnection() as HttpURLConnection
         return try {
             urlConnection2.inputStream.bufferedReader().readText()
         } catch (t: Throwable) {
+            //No UI interactions here, simply throw
             println("соединение отстутсвует")
             null
         }
