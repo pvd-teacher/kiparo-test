3801be1c52ec410c29a39deb19cf3bab4718e392
PVD-Teacher <127093027+pvd-teacher@users.noreply.github.com>
2023-03-21 17:52:20 +0530

    Update DateFormat.kt

--- 01 first test task/app/src/main/java/com/yakovsn/homework/DateFormat.kt
+++ 01 first test task/app/src/main/java/com/yakovsn/homework/DateFormat.kt
@@ -5,11 +5,14 @@ import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+//Singletone too much, it could be simply kotlin file with util functions
 object DateFormat {
     @RequiresApi(Build.VERSION_CODES.O)
     fun dateForm(dateString: String?): String {
+        //private const DATE_FORMAT_...
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss [.SSS]Z")
         val date = LocalDateTime.parse(dateString, formatter)
+        //private const DATE_FORMAT_...
         val newFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
         return date.format(newFormatter)
     }
